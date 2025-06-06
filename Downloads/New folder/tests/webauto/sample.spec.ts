5121622655f503913d5d188d6f9fddfa59e52142
VigneshElumalai14 <vigneshelumalai14@gmail.com>
2025-06-06 13:40:59 +0530

    Signup and Login Functionality

---
+++ Downloads/New folder/tests/webauto/sample.spec.ts
@@ -0,0 +1,10 @@
+import{test, chromium} from '@playwright/test'
+
+test("TC_01",async()=>
+{
+    let browser = await chromium.launch();
+    let context=await browser.newContext();
+    let page=await context.newPage();
+    await page.goto("https://www.automationexercise.com/signup");
+
+});
